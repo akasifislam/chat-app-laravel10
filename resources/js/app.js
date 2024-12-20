b1e64afe19fa50dbee4ad745ebd0a96799cff1a6
akasifislam <aseasifislam@gmail.com>
2024-12-20 23:59:13 +0600

    chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import { createApp } from 'vue';
+import MainApp from './components/MainApp.vue';
+
+createApp(MainApp).mount('#app');
+
+
 window.Alpine = Alpine;
 
 Alpine.start();
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel with Vue 3</title>
+    @vite(['resources/js/app.js', 'resources/scss/style.scss'])
+</head>
+
+<body>
+    <div id="app">
+        <main-app></main-app>
     </div>
-</x-app-layout>
+</body>
+
+</html>
